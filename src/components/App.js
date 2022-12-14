7b14cb2f508f07218962a6f5a15ef65bc4ccd0c6
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-12-14 09:10:45 +0300

    introduce MUI components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react"
-import { Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@mui/material"
+import { Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar, Container, Button, ButtonGroup } from "@mui/material"
 import { PhotoCamera } from "@mui/icons-material"
+
+
 
 export default function App() {
     return (
@@ -14,15 +16,30 @@ export default function App() {
         </AppBar>
         <main>
             <div>
-                <Container maxWidth="sm">
+                <Container maxWidth="sm" style={{padding: '50px'}}>
                     <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                             Photo Album
                     </Typography>
                     <Typography variant="h5" align="center" color="textSecondary">
                             Hello everyone! This is a photo album Page. Checking to see how a long sentence would look like.
                     </Typography>
+                    <div>
+                        <Grid container spacing={2} justifyContent="center">
+                            <Grid item>
+                                <Button variant="contained">
+                                    See My Photos
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="outlined" color="secondary">
+                                    Secondary Action
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </div>
                 </Container>
             </div>
+            
         </main>
        </>
     )
