91134f17cfc46ea521572cefcc12c85edfcac896
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-12-15 21:12:49 +0300

    button + typography

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react"
+import { Button, Typography } from "@mui/material"
 
 export default function App() {
     return (
-        <h1>Hello World!</h1>
+        <>
+            <Button variant="contained">Hello World!</Button>
+            <Typography variant="h4">This is a text</Typography>
+        </>
     )
 }
